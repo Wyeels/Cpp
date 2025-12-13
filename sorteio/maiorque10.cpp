9f9c0fdd6863c4b33f5ab5b4666e132d71386093
Wesley Oliveira Xavier <143528390+Wyeels@users.noreply.github.com>
2025-12-13 13:45:59 -0300

    Create maiorque10.cpp

---
+++ sorteio/maiorque10.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int maiorque() {
+    int valor;
+    cin >> valor;
+
+    if (valor > 10) {
+        cout << "E maior que 10!" << endl;
+
+    } else {
+        cout << "Nao e maior que 10" << endl;
+    }
+}
