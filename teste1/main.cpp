91574948596fadcd1f5af515cf76801375899e0c
Wesley Oliveira Xavier <143528390+Wyeels@users.noreply.github.com>
2025-12-13 13:15:29 -0300

    modificando algumas coisas

--- teste1/main.cpp
+++ teste1/main.cpp
@@ -1,10 +1,31 @@
+// Online C++ compiler to run C++ program online
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main() {
-    int idade;
-    cout << "Digite sua idade:";
-    cin >> idade;
-    cout << "Você nasceu em " << 2025 - idade << endl;
-    return 0;
+    vector<int>list1(2);
+    vector<int>list2(2);
+    for (int i = 0; i < 2; i++) {
+      cin >> list1[i];
+    }
+    for (int i = 0; i < 2; i++) {
+      cin >> list2[i];
+    }
+
+    
+    vector<int>final(50);
+    for (int i = 0; i < 50; i++) {
+      for (int c = 0; c < 50; c++) {
+        if (list1[i] == c || list2[i] == c) {
+        final[c] = i;
+        }
+      }
+  }
+
+  for (int i = 0; i < 4; i++) {
+    cout << final[i] << endl;
+  }
+        
+  return 0;
 }
